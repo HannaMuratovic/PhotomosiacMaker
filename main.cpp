3532908f6ec07d449785ee9b4f71e670c7160cec
HannaMuratovic <H2999MUR@uwaterloo.ca>
2019-01-01 18:41:59 -0500

    First code commit

--- main.cpp
+++ main.cpp
@@ -9,9 +9,33 @@ struct tile {
   int average[3];
 }
 
-void rgb_average(Mat image, int &average[3]){
-
+void rgb_average(Mat image, int &tile_average[3]){
+  int image_size = image.cols*image.rows;
+  int blue, green, red = 0;
+  for (int j = 1; j <= image.cols; j++) {
+    for (int k = 1; k <= image.rows; k++) {
+      Vec3b intensity = image.at<Vec3b>(k, j);
+      blue += intensity.val[0];
+      green += intensity.val[1];
+      red += intensity.val[2];
+    }
+  }
+  tile_average[0] = red/image_size;
+  tile_average[1] = green/image_size;
+  tile_average[2] = blue/image_size;
 }
+
+void create_main_tiles(Mat image, vector<int> &main_tile_averages, int td){
+  for (int i = 0; i <= image.cols; i += td){
+    for (int j = 0; j <= image.rows; j += td){
+      Rect roi = Rect(i*td, j*td, (i+1)*td, (j+1)*td);
+      Mat image_roi = image(roi);
+      
+    }
+  }
+}
+
+
 
 void main(){
    const char* directory_name = "C:\\Users\\HANNA\\Desktop\\Photos\\Dorm Photos";
@@ -21,9 +45,15 @@ void main(){
    Mat tile_image;
    tile tile_data;
    tile store_tiles[file_names.size()];
-   for (int i = 0; i < file_names.size(); i++) {
+   for (int i = 0; i < file_names.size(); i++){
+     tile_data.filepath = file_names[i];
      tile_image = imread(file_names[i], 1);
      tile.filepath = file_names[i];
-     rgb_average(tile_image, tile.average);
+     rgb_average(tile_image, tile_data.average);
+     store_tiles[i] = tile_data;
     }
+
+    string main_image_path = "";
+    Mat main_image = imread(main_image_path);
+
 }
